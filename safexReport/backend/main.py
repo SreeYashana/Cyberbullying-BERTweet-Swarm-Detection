2ca56897a497fa36a0436f48de107a5f36d1c76b
Sree Yashana <sreeyashana4@gmail.com>
2026-03-27 21:19:30 +0530

    fix for render

--- safexReport/backend/main.py
+++ safexReport/backend/main.py
@@ -35,6 +35,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:3000",
         "http://localhost:3000",
+        "https://sreeyashana.github.io"
     ],
     allow_credentials=True,
     allow_methods=["*"],
